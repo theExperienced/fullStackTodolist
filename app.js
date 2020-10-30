fbc475cfddcce94363aff5ff3506bd33cc74e4f5
theExperienced <mizradan2@gmail.com>
2020-10-30 17:04:16 +0200

    fixed express.static()

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 app.use('/todos', todosRouter);
 
 app.get("*", (req, res) => {
-    res.send('helllo');
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 
